ebc98ceaa99a7ca15f2228b4d9da0edca9d60539
sauryaaman <amansaurya24@gmail.com>
2025-09-18 11:01:31 +0530

    add domain &subdomain

--- backend/src/app.js
+++ backend/src/app.js
@@ -26,7 +26,9 @@ app.get('/api/health', async (req, res) => {
 
 
 const allowedOrigins = [
-  "https://bireena-atithi.vercel.app"    // prod frontend
+  "https://bireena-atithi.vercel.app",
+   "https://www.bireenaatithi.in",
+    "https://bireenaatithi.in"    // prod frontend
 ];
 
 app.use(cors({
